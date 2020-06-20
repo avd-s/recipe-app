fc698cfb7950e08c9ff64af8a04d4c0d34694ca0
avd-s <spawnet.11@gmail.com>
2020-06-20 23:12:42 +0530

    Updates

---
+++ precache-manifest.1b542313a4b7ad5146d91ad807409409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb458b0bf1faac121a9bd57319d7c3a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b767722717eed5903aa9",
+    "url": "/static/css/main.ae7b27c9.chunk.css"
+  },
+  {
+    "revision": "0f022d5dfcea7da32f88",
+    "url": "/static/js/2.fb7cd7fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb7cd7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b767722717eed5903aa9",
+    "url": "/static/js/main.863ccf7b.chunk.js"
+  },
+  {
+    "revision": "f6c136e39d74ebdf7280",
+    "url": "/static/js/runtime-main.121956fb.js"
+  }
+]);
